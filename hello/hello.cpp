433df0612ddafa37e7fc74ccfaa432d34f6a4c52
xukang <xukang@chukong-inc.com>
2019-04-25 14:36:01 +0800

    modify hello scripts

--- hello/hello.cpp
+++ hello/hello.cpp
@@ -2,28 +2,15 @@
 
 using namespace eosio;
 
-int g_b = 2;
-
 class [[eosio::contract]] hello : public contract {
     public:
         using contract::contract;
 
         [[eosio::action]]
             void hi(name user) {
-                g_b++;
-                m_a++;          
-                require_auth(user);
-                print("[hi] hello, ", name{user}, ", m_a = ", m_a, ", g_b = ", g_b);
+                print("user: ", user);
+                check(false, "hello, constract");
             }
-
-        [[eosio::action]]
-            void say(name user) {
-                m_a += 100;          
-                require_auth(user);
-                print("[say] hello, ", name{user}, ", m_a = ", m_a, ", g_b = ", g_b);
-            }
-    private:
-        int m_a = 0;
 };
 
 
